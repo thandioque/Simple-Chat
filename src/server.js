4d1e0636ed325ccb39606f029d3fbf4910e6b0a2
tolivei1 <ThandiweAssis.OliveiraFideles@windriver.com>
2023-03-17 14:58:44 -0300

    Fix savedMessage error

--- src/server.js
+++ src/server.js
@@ -1,71 +1,63 @@
-var express = require('express');
+var express = require('express')
 var bodyParser = require('body-parser')
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var mongoose = require('mongoose');
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-var Message = mongoose.model('Message',{
-  name : String,
-  message : String
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String,
 })
 
-var mongoURL = process.env.MONGODB_URI || 'mongodb://mongouser:mongopwd@mongo:27017/admin';
-var port = process.env.PORT || '3000';
+var mongoURL =
+    process.env.MONGODB_URI || 'mongodb://mongouser:mongopwd@mongo:27017/admin'
+var port = process.env.PORT || '3000'
 
 app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
-  })
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
 })
-
 
 app.get('/messages/:user', (req, res) => {
-  var user = req.params.user
-  Message.find({name: user},(err, messages)=> {
-    res.send(messages);
-  })
+    var user = req.params.user
+    Message.find({ name: user }, (err, messages) => {
+        res.send(messages)
+    })
 })
-
 
 app.post('/messages', async (req, res) => {
-  try{
-    var message = new Message(req.body);
+    try {
+        var message = new Message(req.body)
 
-    var savedMessage = await message.save()
-      console.log('saved');
+        await message.save()
+        console.log('saved');
 
-    var censored = await Message.findOne({message:'badword'});
-      if(censored)
-        await Message.remove({_id: censored.id})
-      else
-        io.emit('message', req.body);
-      res.sendStatus(200);
-  }
-  catch (error){
-    res.sendStatus(500);
-    return console.log('error',error);
-  }
-  finally{
-    console.log('Message Posted')
-  }
-
+        var censored = await Message.findOne({ message: 'badword' })
+        if (censored) await Message.remove({ _id: censored.id })
+        else io.emit('message', req.body)
+        res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500)
+        return console.log('error', error)
+    } finally {
+        console.log('Message Posted')
+    }
 })
 
-
-
-io.on('connection', () =>{
-  console.log('a user is connected')
+io.on('connection', () => {
+    console.log('a user is connected')
 })
 
-mongoose.connect(mongoURL ,{useNewUrlParser: true} ,(err) => {
-  console.log('mongodb connected',err);
+mongoose.connect(mongoURL, { useNewUrlParser: true }, (err) => {
+    console.log('mongodb connected', err)
 })
 
 var server = http.listen(port, () => {
-  console.log('server is running on port', server.address().port);
-});
+    console.log('server is running on port', server.address().port)
+})
